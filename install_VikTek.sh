04189c1a5379694d7b9a180be36f8038db5e1da8
ccantin <ccantin@student.le-101.fr>
2019-06-07 18:54:16 +0200

    Install script linux need test

---
+++ install_VikTek.sh
@@ -0,0 +1,23 @@
+# get os 
+os=`uname -a`
+case $os in 
+	Darwin*)
+	git clone https://github.com/kube/42homebrew.git
+	sh ./42homebrew/install.sh
+	source ~/.zshrc
+	brew install sdl2
+	brew install sdl2_image
+	brew install sdl2_ttf
+	brew install cmake
+	;;
+	Linux*)
+	sudo -i apt-get install libsdl2-dev
+	sudo -i apt-get install libsdl2-image2.0 libsdl2-image2-dev
+	sudo -i apt-get install cmake
+	;;
+	*)
+	;;
+esac
+	git clone https://github.com/gmonacho/Build_Vik_Wrap.git
+	git clone https://github.com/gmonacho/VikTek2d.git
+
